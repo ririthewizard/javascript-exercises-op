9800735cfc6d185650ce6da223e1e4a06ed18c07
Riley Williams <ririwilliams.rw@gmail.com>
2024-07-17 11:02:28 -0500

    Messing around with DOM manipulation. Starting to get into anonymous and named funcs and attaching them to event listeners.

--- dom-manipulation-p1/script.js
+++ dom-manipulation-p1/script.js
@@ -1,5 +1,7 @@
 const container = document.querySelector("#container");
 
+
+
 const content = document.createElement("div");
 content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
@@ -11,3 +13,30 @@ p.textContent = "Hey I'm red!";
 p.style.color = "red";
 
 container.appendChild(p);
+
+const hThree = document.createElement("h3");
+hThree.textContent = "I'm a blue h3!";
+hThree.style.color = "blue";
+
+container.appendChild(hThree);
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+    alert("Hello World");
+});
+btn.addEventListener("click", function (e) {
+    e.target.style.background = "blue";
+})
+
+// const pinkDiv = document.createElement("div");
+// pinkDiv.classList.add("pinkDiv");
+// pinkDiv.style.backgroundColor = "pink";
+// pinkDiv.style.borderColor = "black";
+
+// const hOne = document.createElement("h1");
+// hOne.textContent("I'm in a div");
+// pinkDiv.appendChild(hOne);
+
+// container.appendChild(pinkDiv);
+
+
