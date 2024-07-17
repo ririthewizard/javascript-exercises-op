03e4234a9e6ac120174118726891f095efef8e57
Riley Williams <ririwilliams.rw@gmail.com>
2024-07-17 11:09:51 -0500

    Changed things to be more streamlined. Used a forEach func to iterate over the buttons and alert

--- dom-manipulation-p1/script.js
+++ dom-manipulation-p1/script.js
@@ -1,6 +1,12 @@
 const container = document.querySelector("#container");
 
+const buttons = document.querySelectorAll("button");
 
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        alert(button.id);
+    });
+});
 
 const content = document.createElement("div");
 content.classList.add("content");
@@ -19,24 +25,5 @@ hThree.textContent = "I'm a blue h3!";
 hThree.style.color = "blue";
 
 container.appendChild(hThree);
-
-const btn = document.querySelector("#btn");
-btn.addEventListener("click", () => {
-    alert("Hello World");
-});
-btn.addEventListener("click", function (e) {
-    e.target.style.background = "blue";
-})
-
-// const pinkDiv = document.createElement("div");
-// pinkDiv.classList.add("pinkDiv");
-// pinkDiv.style.backgroundColor = "pink";
-// pinkDiv.style.borderColor = "black";
-
-// const hOne = document.createElement("h1");
-// hOne.textContent("I'm in a div");
-// pinkDiv.appendChild(hOne);
-
-// container.appendChild(pinkDiv);
 
 
