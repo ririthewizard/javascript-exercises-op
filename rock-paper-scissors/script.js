7113a387c196f041aa38b56a7ffd50d46ca330fd
Riley Williams <ririwilliams.rw@gmail.com>
2024-07-17 14:46:07 -0500

    Figured out what I needed to do with createElement and am working on having a permanent score display, and a separate text field for displaying the choices and whether you won or lost that round. Next after that is creating the win condition

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,22 +1,33 @@
 const gameChoices = ["rock", "paper", "scissors"];
 let humanScore = 0;
 let computerScore = 0;
-const displayResults = document.querySelector("displayResults");
+let humanInput;
+
+const gameGrid = document.querySelector("#game");
+const displayResults = document.createElement("h3");
+const humanResults = document.createElement("h1");
+const computerResults = document.createElement("h1");
+
+gameGrid.append(humanResults, computerResults, displayResults);
 // document.body.appendChild(displayResults);
 
+const handleClick = (e) => {
+    humanInput = e.target.id;
+    humanResults.textContent = "Your choice: " + humanInput;
+    playRound(humanInput, getComputerChoice());
+}
 
 function getComputerChoice() {
     return gameChoices[Math.floor(3 * Math.random())];
 }
 
-function getHumanChoice() {
-    let humanInput = prompt("Choose rock, paper, or scissors");
-    if (gameChoices.includes(humanInput.toLowerCase())) {
-        return humanInput.toLowerCase();
-    }
-    return console.log("You didn't choose rock, paper, or scissors");
+for (let i = 0; i < gameChoices.length; i++) {
+    const button = document.createElement("button");
+    button.id = gameChoices[i];
+    button.textContent = gameChoices[i];
+    button.addEventListener("click", handleClick);
+    gameGrid.appendChild(button);
 }
-
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice.toLowerCase() === computerChoice) {
@@ -26,7 +37,7 @@ function playRound(humanChoice, computerChoice) {
         (humanChoice.toLowerCase() === "paper" && computerChoice === "rock") ||
         (humanChoice.toLowerCase() === "scissors" && computerChoice === "paper")) {
         humanScore++;
-        displayResults.textContent = "Computer chose " + computerChoice + ", you win!"; 
+        displayResults.textContent = "Human score: " + humanScore + " | Computer score: " + computerScore;
     } else {
         computerScore++;
         displayResults.textContent = "You lose! Computer chose " + computerChoice;
@@ -34,29 +45,29 @@ function playRound(humanChoice, computerChoice) {
 }
 
 
-const buttonRock = document.createElement("button");
-buttonRock.textContent = "Rock";
-document.body.appendChild(buttonRock);
+// const buttonRock = document.createElement("button");
+// buttonRock.textContent = "Rock";
+// document.body.appendChild(buttonRock);
 
-const buttonPaper = document.createElement("button");
-buttonPaper.textContent = "Paper";
-document.body.appendChild(buttonPaper);
+// const buttonPaper = document.createElement("button");
+// buttonPaper.textContent = "Paper";
+// document.body.appendChild(buttonPaper);
 
-const buttonScissors = document.createElement("button");
-buttonScissors.textContent = "Scissors";
-document.body.appendChild(buttonScissors);
+// const buttonScissors = document.createElement("button");
+// buttonScissors.textContent = "Scissors";
+// document.body.appendChild(buttonScissors);
 
-buttonRock.addEventListener("click", () => {
-    playRound("Rock", getComputerChoice());
-});
+// buttonRock.addEventListener("click", () => {
+//     playRound("Rock", getComputerChoice());
+// });
 
-buttonPaper.addEventListener("click", () => {
-    playRound("Paper", getComputerChoice());
-});
+// buttonPaper.addEventListener("click", () => {
+//     playRound("Paper", getComputerChoice());
+// });
 
-buttonScissors.addEventListener("click", () => {
-    playRound("Scissors", getComputerChoice());
-});
+// buttonScissors.addEventListener("click", () => {
+//     playRound("Scissors", getComputerChoice());
+// });
 
 
 
