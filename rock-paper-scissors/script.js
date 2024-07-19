31ac6571fc6f45a72de1fb6b79830d412ad06515
Riley Williams <ririwilliams.rw@gmail.com>
2024-07-19 11:00:44 -0500

    Finished up a few things. One, added a running score element. Two, finished the game logic by checking if one score or the other is 5 and returning if that is the case

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -2,19 +2,23 @@ const gameChoices = ["rock", "paper", "scissors"];
 let humanScore = 0;
 let computerScore = 0;
 let humanInput;
+let computerInput;
 
 const gameGrid = document.querySelector("#game");
-const displayResults = document.createElement("h3");
-const humanResults = document.createElement("h1");
-const computerResults = document.createElement("h1");
+const displayResults = document.createElement("h1");
+const humanResults = document.createElement("h3");
+const computerResults = document.createElement("h3");
+const overallScore = document.createElement("h1");
 
-gameGrid.append(humanResults, computerResults, displayResults);
-// document.body.appendChild(displayResults);
+gameGrid.append(humanResults, computerResults, displayResults, overallScore);
+overallScore.textContent = "Human score: 0 | Computer score: 0";
 
 const handleClick = (e) => {
     humanInput = e.target.id;
+    computerInput = getComputerChoice();
     humanResults.textContent = "Your choice: " + humanInput;
-    playRound(humanInput, getComputerChoice());
+    computerResults.textContent = "Computer choice: " + computerInput;
+    playRound(humanInput, computerInput);
 }
 
 function getComputerChoice() {
@@ -32,42 +36,22 @@ for (let i = 0; i < gameChoices.length; i++) {
 function playRound(humanChoice, computerChoice) {
     if (humanChoice.toLowerCase() === computerChoice) {
         displayResults.textContent = "It's a tie!";
+        overallScore.textContent = "Human score: " + humanScore + " | Computer score: " + computerScore;
         return;
     } else if ((humanChoice.toLowerCase() === "rock" && computerChoice === "scissors") ||
         (humanChoice.toLowerCase() === "paper" && computerChoice === "rock") ||
         (humanChoice.toLowerCase() === "scissors" && computerChoice === "paper")) {
         humanScore++;
-        displayResults.textContent = "Human score: " + humanScore + " | Computer score: " + computerScore;
+        displayResults.textContent = "You win! Computer chose " + computerChoice;
+        overallScore.textContent = "Human score: " + humanScore + " | Computer score: " + computerScore;
     } else {
         computerScore++;
         displayResults.textContent = "You lose! Computer chose " + computerChoice;
+        overallScore.textContent = "Human score: " + humanScore + " | Computer score: " + computerScore;
+    }
+    if (humanScore === 5) {
+        displayResults.textContent = "CONGRATS! You win! You reached 5 points first.";
+    } else if (computerScore === 5) {
+        displayResults.textContent = "BOO! Better luck next time. You lose. Computer reached 5 points first";
     }
 }
-
-
-// const buttonRock = document.createElement("button");
-// buttonRock.textContent = "Rock";
-// document.body.appendChild(buttonRock);
-
-// const buttonPaper = document.createElement("button");
-// buttonPaper.textContent = "Paper";
-// document.body.appendChild(buttonPaper);
-
-// const buttonScissors = document.createElement("button");
-// buttonScissors.textContent = "Scissors";
-// document.body.appendChild(buttonScissors);
-
-// buttonRock.addEventListener("click", () => {
-//     playRound("Rock", getComputerChoice());
-// });
-
-// buttonPaper.addEventListener("click", () => {
-//     playRound("Paper", getComputerChoice());
-// });
-
-// buttonScissors.addEventListener("click", () => {
-//     playRound("Scissors", getComputerChoice());
-// });
-
-
-
