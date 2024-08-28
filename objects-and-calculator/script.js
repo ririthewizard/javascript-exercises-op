48118e83e0238bef660e226a473ca4e0fd34a098
Riley Williams <ririwilliams.rw@gmail.com>
2024-08-27 19:47:07 -0500

    Starting to work on array methods and objects

---
+++ objects-and-calculator/script.js
@@ -0,0 +1,14 @@
+function sumOfTripledEvens(array) {
+    let evenArray = array.filter((num) => num % 2 === 0);
+    // console.log(evenArray);
+
+    let tripledArray = evenArray.map((num) => num * 3);
+    // console.log(tripledArray);
+
+    let summedArray = tripledArray.reduce((total, currentItem) => {
+        return total + currentItem;
+    });
+    console.log(summedArray);
+}
+let arr = [1, 2, 3, 4, 5, 6];
+sumOfTripledEvens(arr);
